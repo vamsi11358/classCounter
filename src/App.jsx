d0df158859f3c1cf7fb0298131f43ea58916b3d4
vamsi11358 <vamsi11358@gmail.com>
2024-07-15 19:42:46 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {  useState } from 'react'
+import Component from "./component/component";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Component/>
+        
+    </>
+  )
+}
+
+export default App
